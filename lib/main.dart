43b5302111f6cd7422df0e053dba76a4f9ce12fb
noobshubham <noobshubham65@gmail.com>
2022-11-21 21:31:03 +0530

    add: Image and Button + Scrollable

--- lib/main.dart
+++ lib/main.dart
@@ -21,23 +21,60 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class WelcomePage extends StatelessWidget {
+String imagePath1 = 'images/joker.png';
+String imagePath2 = 'images/yeah.png';
+String currentPath = imagePath1;
+
+class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
 
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Welcome'),
+        title: const Text('Welcome App in Flutter'),
         centerTitle: true,
         backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: Image.asset('images/namaste.jpg'),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10.0,
+              ),
+              ElevatedButton(
+                style:
+                    ElevatedButton.styleFrom(backgroundColor: Colors.redAccent),
+                onPressed: () {
+                  setState(() {
+                    if (currentPath == imagePath1) {
+                      currentPath = imagePath2;
+                    } else {
+                      currentPath = imagePath1;
+                    }
+                  });
+                },
+                child: const Text('CLICK'),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Column(
+                children: List.generate(
+                  4,
+                  (index) => Image.asset(currentPath),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-
-
-
