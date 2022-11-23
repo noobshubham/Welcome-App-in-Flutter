581422cee272ab8ddc14bcbb0cd02a8b5a0432f9
noobshubham <noobshubham65@gmail.com>
2022-11-23 20:02:10 +0530

    add: Solution for Login & Register Page

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+        title: const Text('Register Page'),
+      ),
+    );
+  }
+}
